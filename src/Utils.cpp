56f435d9bd55017f6f1c1001975efcd2718b922e
Ken Katagiri <kenk42292@gmail.com>
2020-04-26 17:00:19 -0700

    Added second maze for testing. Fixed small bug in Utils::distance

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -4,5 +4,5 @@
 #include "../include/Utils.h"
 
 long pacman::Utils::distance(std::pair<long, long> a, std::pair<long, long> b) {
-  return sqrt(pow(a.first - b.first, 2) + pow(b.second - b.second, 2));
+  return sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main(void)
 {    
     // TODO: Update to be non-absolute, so it can run on other computers...
-    std::string gameConfigFolderPath = "/home/ken/udacity/pacman/res/gameconfigs/game1";
+    std::string gameConfigFolderPath = "/home/ken/udacity/pacman/res/gameconfigs/game2";
     std::string imgFolderPath = "/home/ken/udacity/pacman/res/img";
 
     // Must create shared_ptr to this intended singleton, to allow use of shared_from_this.
